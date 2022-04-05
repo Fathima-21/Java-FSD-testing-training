ff167d52f6437dddbb03e72c0a26900e28378e0e
Fathima-21 <94165835+Fathima-21@users.noreply.github.com>
2022-04-05 10:36:51 +0530

    Add files via upload

---
+++ JAVA/day2/IfExample2.java
@@ -0,0 +1,22 @@
+package day2;
+
+public class IfExample2 {
+
+	public static void main(String[] args) {
+		System.out.println("start of the program");
+		int a=22,b=45,c=56;
+		if(a==b) {
+			System.out.println("a equal to b");
+		}
+		else if(a==c){
+			System.out.println("a equal to b");
+		}
+		else if(b==c) {
+			System.out.println("b equal to c");
+		}
+		else {
+			System.out.println("none of them are equal");
+		}
+	}
+
+}
