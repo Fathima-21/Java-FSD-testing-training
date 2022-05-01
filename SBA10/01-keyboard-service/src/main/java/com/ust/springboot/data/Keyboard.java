8b648e8a4aeaf489548bb3c09517556e013e7387
Fathima-21 <94165835+Fathima-21@users.noreply.github.com>
2022-05-01 13:11:03 +0530

    Add files via upload

---
+++ SBA10/01-keyboard-service/src/main/java/com/ust/springboot/data/Keyboard.java
@@ -0,0 +1,39 @@
+package com.ust.springboot.data;
+
+public class Keyboard {
+	private int id;
+	private String brand;
+	private int price;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+/*
+	public Keyboard(int id, String brand, int price) {
+		super();
+		this.id = id;
+		this.brand = brand;
+		this.price = price;
+	}
+*/
+}
